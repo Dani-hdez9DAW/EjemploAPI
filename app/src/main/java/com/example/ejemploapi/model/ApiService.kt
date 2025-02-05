ba33748924151792f5cc0922b72ef091a127c354
Daniel <danielhdezgarcia9daw@gmail.com>
2025-02-05 20:06:31 +0100

    Cambios

--- app/src/main/java/com/example/ejemploapi/model/ApiService.kt
+++ app/src/main/java/com/example/ejemploapi/model/ApiService.kt
@@ -1,18 +1,14 @@
-package com.example.ejemploapi.model;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
+import com.example.ejemploapi.model.Language
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 public interface ApiService {
-  @Headers({
-          "Authorization: Bearer <a13bfad9c4535d7b15a803c8383e3a8c>",
-          "Content-Type: application/json"
-  })
-  @GET("languages")
-  Call<List<Language>> getLanguages();
+    @Headers(
+        "Authorization: Bearer a13bfad9c4535d7b15a803c8383e3a8c",
+        "Content-Type: application/json"
+    )
+    @GET("languages")
+    suspend fun getLanguages(): List<Language>
 }
 
-// Otras peticiones...
